14e7982a483e8e9b64ea15d9ab96a0efe75134e1
varun <mehtavarun.47@gmail.com>
2022-03-15 23:26:41 +0530

    cm-1 in master

---
+++ service1/app/Hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import request, Response
+from flask_cors import CORS, cross_origin
+import json
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+@app.route('/', methods=['GET', 'POST'])
+def assetcompliance():
+    if request.method == 'GET':
+        return "Root Page"
+
+@app.route('/hello', methods=['GET', 'POST'])
+def assetcompliance_hello():
+    if request.method == 'GET':
+        return "Hello World Page service 1 master branch"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5052)
